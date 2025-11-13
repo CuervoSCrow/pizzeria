74ab81c6cbd6a028c304bd902f0afb68c0714ff4
Cuervo <vidalcuervo@gmail.com>
2025-11-13 09:25:35 -0600

    clase 8 save con spring repositories

--- src/main/java/com/paltzi/pizza/web/controller/PizzaController.java
+++ src/main/java/com/paltzi/pizza/web/controller/PizzaController.java
@@ -2,12 +2,10 @@ package com.paltzi.pizza.web.controller;
 
 import com.paltzi.pizza.persistencia.entity.PizzaEntity;
 import com.paltzi.pizza.service.PizzaService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +27,23 @@ public class PizzaController {
     @GetMapping("/{idPizza}")
     public ResponseEntity<PizzaEntity> get(@PathVariable int idPizza){
         return ResponseEntity.ok(this.pizzaService.get(idPizza));
+    }
+
+    @PostMapping
+    public ResponseEntity<PizzaEntity> add(@RequestBody PizzaEntity pizza){
+        pizza.setIdPizza(null);
+        if(pizza.getIdPizza() == null || !this.pizzaService.exists(pizza.getIdPizza())){
+            System.out.println("Guardando nueva pizza");
+            return ResponseEntity.ok(this.pizzaService.save(pizza));
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<PizzaEntity> update(@RequestBody PizzaEntity pizza){
+        if(pizza.getIdPizza() != null && this.pizzaService.exists(pizza.getIdPizza())){
+            return ResponseEntity.ok(this.pizzaService.save(pizza));
+        }
+        return ResponseEntity.badRequest().build();
     }
 }
